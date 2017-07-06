0be5b948d0b698762e6d1feef44774ccf51c1c1a
Vasrek <theo.denanassy@epitech.eu>
2017-07-06 16:59:19 +0200

    add timer

--- Assets/Scripts/Projectiles.cs
+++ Assets/Scripts/Projectiles.cs
@@ -8,32 +8,66 @@ public class Projectiles : MonoBehaviour {
     public GameObject Bullet_Emitter;
     //Drag in the Bullet Prefab from the Component Inspector.
     public GameObject Bullet;
+    public GameObject Bomb;
     //Enter the Speed of the Bullet from the Component Inspector.
     public float Bullet_Forward_Force;
     public float Timer = 10.0f;
+
+    private int myRandomBall;
 
     void Update()
     {
         Timer -= Time.deltaTime;
         if ((int)Timer <= 0)
         {
-            //The Bullet instantiation happens here.
-            GameObject Temporary_Bullet_Handler;
-            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+            myRandomBall = Random.Range(0, 2);
+            Debug.Log(myRandomBall);
+            switch (myRandomBall)
+            {
+                case 1:
+                    Debug.Log("Balls");
+                    //The Bullet instantiation happens here.
+                    GameObject Temporary_Bullet_Handler;
+                    Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
 
-            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+                    //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+                    //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+                    Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
 
-            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
-            Rigidbody Temporary_RigidBody;
-            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+                    //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+                    Rigidbody Temporary_RigidBody;
+                    Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
 
-            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
-            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+                    //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+                    Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
 
-            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
-            Destroy(Temporary_Bullet_Handler, 10.0f);
+                    //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+                    Destroy(Temporary_Bullet_Handler, 10.0f);
+                    break;
+                case 0:
+                    Debug.Log("Bomb");
+                    //The Bullet instantiation happens here.
+                    GameObject Temporary_Bullet_Handler2;
+                    Temporary_Bullet_Handler2 = Instantiate(Bomb, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+                    //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+                    //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+                    Temporary_Bullet_Handler2.transform.Rotate(Vector3.left * 90);
+
+                    //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+                    Rigidbody Temporary_RigidBody2;
+                    Temporary_RigidBody2 = Temporary_Bullet_Handler2.GetComponent<Rigidbody>();
+
+                    //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+                    Temporary_RigidBody2.AddForce(transform.forward * Bullet_Forward_Force);
+
+                    //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+                    Destroy(Temporary_Bullet_Handler2, 10.0f);
+                    break;
+                default:
+                    break;
+            }
+           
             Timer = 10.0f;
         }
     }
---
+++ Assets/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timer;
+
+    private float t = 60.0f;
+
+    private enum States
+    {
+        timerOn, timerOff
+    };
+    private States myState;
+
+    private void Start()
+    {
+        myState = States.timerOn;
+    }
+
+    private void Update()
+    {
+        Debug.Log(myState);
+        if (myState == States.timerOn) { StartGame(); }
+        else if (myState == States.timerOff) { endGame(); }
+    }
+
+    void StartGame()
+    {
+        string data = "Temps restant : ";
+
+        timer.text = data + (int)t;
+        t -= Time.deltaTime;
+        if ((int)t == 0)
+            myState = States.timerOff;
+    }
+
+    void endGame()
+    {
+        Debug.Log("End Game");
+    }
+}
