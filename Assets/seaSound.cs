f49d7bb55fe9f967dbd2f36f40ce97ea7147a967
Vasrek <theo.denanassy@epitech.eu>
2017-07-13 11:56:21 +0200

    Sea Sound

---
+++ Assets/seaSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seaSound : MonoBehaviour {
+
+    private AudioSource SeaSFX;
+
+    // Use this for initialization
+    void Start () {
+        SeaSFX = GetComponent<AudioSource>();
+        SeaSFX.Play();
+    }
+}
