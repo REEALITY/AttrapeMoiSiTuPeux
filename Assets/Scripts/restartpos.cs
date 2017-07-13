99c5ccee478c0e085a9fdc337844149e9b91e4fd
Vasrek <theo.denanassy@epitech.eu>
2017-07-13 16:32:38 +0200

    Fix scene game

---
+++ Assets/Scripts/restartpos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartpos : MonoBehaviour {
+
+    private float x;
+    private float y;
+    private float z;
+    // Use this for initialization
+    void Start () {
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+        z = this.transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(x, y, z);
+	}
+}
