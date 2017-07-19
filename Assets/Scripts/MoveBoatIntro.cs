117bd39141fbbad6255ee194b04b6fe5cc015c1e
Vasrek <theo.denanassy@epitech.eu>
2017-07-19 14:25:53 +0200

    Move boat z

---
+++ Assets/Scripts/MoveBoatIntro.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoatIntro : MonoBehaviour {
+
+    public float MoveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, MoveSpeed * Time.deltaTime);
+    }
+}
